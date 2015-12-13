ca01bf48916d3de2dc088f34d5bea92b4ef470d8
Flint Ory <flintory@gmail.com>
2015-12-13 16:45:32 -0600

    updated install_homebridge to ask for install path

--- install_homebridge.py
+++ install_homebridge.py
@@ -13,12 +13,18 @@ def installConfigFile(configFilePath):
 def installISYDeviceTypes(isyDeviceTypesFilePath):
 	subprocess.call(["sudo cp -X " + isyDeviceTypesFilePath + " /usr/local/lib/node_modules/homebridge-isy-js/node_modules/isy-js"], shell=True)
 
-homebridgeInstallPath = "/Users/flintory"
+homebridgeInstallPath = raw_input("What's the path where you want to install homebridge?")
+homebridgeHomePath = homebridgeInstallPath + "/homebridge"
+print(homebridgeInstallPath)
+print(homebridgeHomePath)
+
 
 installNode(homebridgeInstallPath)
-installConfigFile(homebridgeInstallPath + "/homebridge/config.json")
+installConfigFile(homebridgeHomePath + "/config.json")
 installPlugin("homebridge-http")
 installPlugin("homebridge-isy-js")
 installPlugin("homebridge-nest")
 installPlugin("homebridge-legacy-plugins")
-installISYDeviceTypes(homebridgeInstallPath + "/homebridge/isydevicetypes.json")
+installISYDeviceTypes(homebridgeHomePath + "/isydevicetypes.json")
+
+print "All Done!!"
